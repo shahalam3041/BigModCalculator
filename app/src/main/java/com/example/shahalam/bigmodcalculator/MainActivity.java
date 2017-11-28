d3e129f67b66d27112f9ca0ce65a7c1d9c9636d7
ShahAlam <shahalam3041@gmail.com>
2017-11-28 17:38:50 +0600

    declare variables and find them

--- app/src/main/java/com/example/shahalam/bigmodcalculator/MainActivity.java
+++ app/src/main/java/com/example/shahalam/bigmodcalculator/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.shahalam.bigmodcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int base, power, modular;
+    EditText baseInput, powerInput, modularInput;
+    Button Calculate;
+    TextView displayResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        baseInput = findViewById(R.id.input_base);
+        powerInput = findViewById(R.id.input_power);
+        modularInput = findViewById(R.id.input_modular);
+        Calculate = findViewById(R.id.ButtonID);
+        displayResult = findViewById(R.id.result_TextViewID);
     }
 }
